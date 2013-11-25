c89d3d87fb033c337c942d295ae6156bc8ce4d34
Matt Malone <mattagape@gmail.com>
2013-11-25 13:17:52 -0500

    test app for Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AAAWebsiteHeroku::Application.config.secret_key_base = '20b1c579b37f8d86781a8e2b74abd8a3ca798ad4f788bf9a0829c8dc42e08edf35b51ed43605271f40c8ad13c0f94d0b317e07a833bc73277be8e8b30cdf289d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AAAWebsiteHeroku::Application.config.session_store :cookie_store, key: '_AAA_website_heroku_session'
